0c0b78c0ecaeddaf83b7e4106ae03530800af38b
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-27 23:35:22 +0530

    ✨[Nitish] setup address book appliaction basic needs

---
+++ src/main/java/com/bridelabz/nitish/springaddressbookapp/SpringAddressBookAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridelabz.nitish.springaddressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddressBookAppApplication {
+    //main program for running code.
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAddressBookAppApplication.class, args);
+    }
+
+}
